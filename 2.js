cc39cfc3548fe3994f6c7a1bafc9db79534ea230
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-07-27 21:37:08 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,6 @@
+const sum=(a,b)=>a+b;
+const mul=(a,b)=>a*b;
+const pi=3.14;
+const g=9.18;
+
+module.exports={sum,mul,pi,g}
---
+++ script.js
@@ -0,0 +1,4 @@
+const someValue=require("./2")
+console.log(someValue)
+console.log(someValue.mul(3,4))
+console.log(someValue.sum(3,4))
